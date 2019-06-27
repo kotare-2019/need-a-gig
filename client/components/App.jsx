37f18982705a8033852d9a77d5f5852a742830a3
Tess Meyer <49256315+tess-meyer@users.noreply.github.com>
2019-06-28 11:28:44 +1200

    add internal api functions and backend form stuff

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,66 @@
 import React from 'react'
+import { getUsers, createUser } from '../../client/internal-api';
 
-const App = () => {
+class App extends React.Component {
+
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    this.refreshUsers()
+  }
+
+  refreshUsers = () => {
+    getUsers().then(users => {
+      this.setState({
+        users
+      })
+    })
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    
+    event.preventDefault()
+
+    console.log("form submitted")
+
+    const user = {
+      name: this.state.name,
+      city: this.state.city
+    }
+
+    
+    console.log(user)
+
+    this.setState({
+      name: '',
+      city: ''
+    })
+
+    createUser(user).then(newUser => {
+      this.refreshUsers()
+    })
+  }
+
+  render() {
   return (
    <div> 
      <h1>Do YOU need a gig?</h1>
-     <form>
-          <label>Name: <input type="text" name="Name" /></label>
-          <label>City: <input type="text" name="City" /></label>
+     <form onSubmit={this.handleSubmit}>
+          <label>Name: <input type="text" name="Name" value={this.state.name} onChange={this.handleChange} /></label>
+          <label>City: <input type="text" name="City" value={this.state.city} onChange={this.handleChange} /></label>
           <button>Go!</button>
       </form>
    </div>
   )
+ }
 }
 
 export default App
