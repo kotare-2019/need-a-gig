c38040a291b7da51a944a4bab3805463d4d6f19c
Tess Meyer <49256315+tess-meyer@users.noreply.github.com>
2019-06-28 10:45:46 +1200

    add seed data

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,51 +2,62 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1>Do YOU need a gig?</h1>
+   <div> 
+     <h1>Do YOU need a gig?</h1>
+     <form>
+          <label style={{display: 'block'}}>Name: <input type="text" name="Name" /></label>
+          City: &nbsp;
+          <select>
+          <option value="Wellington">Wellington</option>
+          <option value="Auckland">Auckland</option>
+          </select>
+          <button>Go!</button>
+        </form>
+   </div>
   )
 }
 
 export default App
 //////
 
-import React from 'react'
-import Dragons from './Dragon2'
-import unsplashImage from './UnsplashComponent'
-import { getCapsules } from '../../api/spaceX'
-import { unsplashRandom } from '../../api/unsplashImages'
+// import React from 'react'
+// import Dragons from './Dragon2'
+// import unsplashImage from './UnsplashComponent'
+// import { getCapsules } from '../../api/spaceX'
+// import { unsplashRandom } from '../../api/unsplashImages'
 
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props)
 
-    this.state = {
-      isVisible: false,
-      capsules: [],
-      image: []
-    }
-  }
+//     this.state = {
+//       isVisible: false,
+//       capsules: [],
+//       image: []
+//     }
+//   }
 
-  componentDidMount() {
-    getCapsules()
-      .then(capsules => {
-        this.setState({ capsules: capsules })
-      })
+//   componentDidMount() {
+//     getCapsules()
+//       .then(capsules => {
+//         this.setState({ capsules: capsules })
+//       })
 
-    unsplashRandom()
-      .then(image => {
-        this.setState({ image: image })
-      })
-  }
+//     unsplashRandom()
+//       .then(image => {
+//         this.setState({ image: image })
+//       })
+//   }
 
-  render() {
-    return (
-      <>
-        <h1>SpaceX!</h1>
-        {this.isVisible && <h2>What</h2>}
-        <Dragons allCapsules={this.state.capsules} />
-      </>
-    )
-  }
-}
-export default App
+//   render() {
+//     return (
+//       <>
+//         <h1>SpaceX!</h1>
+//         {this.isVisible && <h2>What</h2>}
+//         <Dragons allCapsules={this.state.capsules} />
+//       </>
+//     )
+//   }
+// }
+// export default App
---
+++ migrations/20190628103102_users.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary()
+    table.string('name')
+    table.string('city')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('widgets')
+};
